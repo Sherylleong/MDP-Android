a4153f7ade046da34ae62d8f130d6d603b747d3a
xavier7252 <xavier_tgy@yahoo.com.sg>
2024-09-02 13:27:50 +0800

    msg func

--- app/src/main/java/com/example/tippy/Message.java
+++ app/src/main/java/com/example/tippy/Message.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
